c2b8b65be9a1b0e6ab206409b8aed89b7250a3e4
JesusamadoF <jesus.amador@unah.hn>
2023-04-25 19:46:09 -0500

    añadiendo ultimos datos

--- src/app.js
+++ src/app.js
@@ -7,9 +7,8 @@ import ventasRoutes from './routes/ventas.routes.js'
 
 
 
-
 const app = express()
- 
+
 app.use(express.json())
 
    app.use(indexRoutes)
@@ -17,8 +16,15 @@ app.use(express.json())
    app.use('/api', juegosRoutes)
    app.use('/api', plataformasRoutes)
    app.use('/api', ventasRoutes)
-   
 
+
+
+   app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*'); // Permite todas las solicitudes de cualquier origen
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos HTTP permitidos
+      res.header('Access-Control-Allow-Headers', 'Content-Type'); // Cabeceras permitidas
+      next();
+    });
  
 
    app.use((req, res, next)=>{
